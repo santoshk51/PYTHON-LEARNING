f4364504c2f2db7e175a434ad1072ad7dbf0f5eb
santosh <santosh220903@gmail.com>
2026-01-03 23:39:17 +0530

    normal method of array done

--- array.py
+++ array.py
@@ -25,3 +25,26 @@ val.insert(2,50)
 
 for i in range(0, len(val)):
 	print(val[i], end = " ")
+
+copyArr = array(val.typecode , (x*2 for x in val))
+
+copyArr.pop()
+
+
+# for i in range(0, len(copyArr)):
+# 	print(copyArr[i], end = " ")
+
+newArr = val[::-1]
+
+# for i in range(0, len(newArr)):
+# 	print(newArr[i], end = " ")
+
+arrCreate = array('i' , [])
+
+n = int(input("Enter any number: "))
+
+for i in range(0, n):
+	arrCreate.append(int(input("Enter the Data: ")))
+
+for x in arrCreate:
+	print(x, end=" ")
