761f337cd335995db3f04d1e01732069f77360b0
santosh <santosh220903@gmail.com>
2026-01-10 22:31:20 +0530

    class constr logic done

---
+++ Day-5/Q-8.py
@@ -0,0 +1,13 @@
+class player:
+	player_count = 0
+	def __init__(self, name, level):
+		self.name = name
+		self.level = level
+		player.player_count += 1
+
+p1 = player("SK", 5)
+p2 = player("RK", 9)
+p3 = player("CK", 7)
+
+print("Total players:", player.player_count)
+		
