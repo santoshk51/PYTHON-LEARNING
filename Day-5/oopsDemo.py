0e480a60b29bf1392db0d3dca37e22ee1e517576
santosh kumar <santosh220903@gmail.com>
2026-01-11 20:12:05 +0530

    Update oopsDemo.py

--- Day-5/oopsDemo.py
+++ Day-5/oopsDemo.py
@@ -8,6 +8,8 @@ class Laptop:
 	def get_info(self):
 		print(f"Laptop has {self.RAM} RAM & {self.storage} storage {self.storage_type} type")
 
+
+
 l1 = Laptop("16GB", "512GB")
 l2 = Laptop("8GB", "256GB")
 l1.get_info()
